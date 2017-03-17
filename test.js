4ecc157d752385a351badf1f0c429ec1fa1ee7a5
Talk Lee <maxwellnotalone@gmail.com>
2017-03-17 18:29:10 +0800

    3.17 18:00

--- test.js
+++ test.js
@@ -536,4 +536,87 @@ function Mailbox(props) {
         document.getElementById("root")
     );
 
+    function NumberList(props) {
+        const numbers = props.numbers;
+        const listItems = number.map((number) => 
+            <ListItem key={number.toString()}
+                value={number} />
+        );
+        return (
+            <ul>
+                {listItems}
+            </ul>
+        );
+    }
+
+    function NumberList(props) {
+        const numbers = props.numbers;
+        return (
+            <ul>
+                {numbers.map((number) => 
+                    <ListItem key={number.toString()}
+                        value={number} />
+                )}
+            </ul>
+        );
+    }
+
+    class NameForm extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = {value: ""};
+            this.handleChange = this.handleChange.bind(this);
+            this.handleSubmit = this.handleSubmit.bind(this);
+        }
+        handleChange(event) {
+            this.setState({value: event.target.value});
+        }
+        handleSubmit(event) {
+            alert("A name " + this.state.value);
+            event.preventDefault();
+        }
+        render() {
+            return (
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Name:
+                        <input type="text" value={this.state.value} onChange={this.handleChange} >
+                    </label>
+                    <input type="submit" value="Submit">
+                </form>
+            )
+        }
+    }
+
+
+    class EssayForm extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                value: 'Please write something'
+            };
+            this.handleChange = this.handleChange.bind(this);
+            this.handleSubmit = this.handleSubmit.bind(this);
+            handleChange(event) {
+                this.setState({value: event.target.value});
+            }
+        }
+
+        render() {
+            return (
+                <form onSubmit ={this.handleSubmit} >
+                <label>
+                    Name: 
+                    <textarea value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+                </form>
+            );
+        }
+    }
+
+
     
+
+
+
